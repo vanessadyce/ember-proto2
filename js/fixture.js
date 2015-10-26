13622b5471da1bb1907ceddaabc054fd3f6a6330
Vanessa Dyce <vanessadyce@Jamies-MacBook-Air.local>
2015-10-26 01:08:31 -0400

    demo proto for ember

---
+++ js/fixture.js
@@ -0,0 +1,41 @@
+var pizzaz = [{
+    'id': 0,
+    'name': 'Psychology',
+    'image': 'imgs/psych.jpg',
+    'description': 'A quest to understand the human mind and behavior '
+}, {
+    'id': 1,
+    'name': 'Mathematics',
+    'image': 'imgs/math.jpg',
+    'description': 'Mathematics is not about numbers and equations '
+}, {
+    'id': 2,
+    'name': 'Physics',
+    'image': 'imgs/physics.jpg',
+    'description': 'Relativity applies to physics, not ethics '
+}, {
+    'id': 3,
+    'name': 'Biology',
+    'image': 'imgs/biology.jpg',
+    'description': 'Evolution is the concept that makes Biology unique '
+},{
+    'id': 4,
+    'name': 'Computer Science',
+    'image': 'imgs/cs.jpg',
+    'description': 'Computer Science is the only discipline'
+}, {
+    'id': 5,
+    'name': 'Quantitative Biology',
+    'image': 'imgs/quant-bio.jpg',
+    'description': 'A cell is regarded as the true biological atom '
+}, {
+    'id': 6,
+    'name': 'Quantitative Finance',
+    'image': 'imgs/quant-fin.jpg',
+    'description': 'Credit and risk'
+}, {
+    'id': 7,
+    'name': 'Statistics',
+    'image': 'imgs/stats.jpg',
+    'description': 'Statistics is the grammar of science'
+}];
